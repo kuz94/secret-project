4f1b443bb98dd10bf968cf5479f0c816133606aa
Kuzmin Anton <kuz942@gmail.com>
2018-03-14 22:26:20 +0300

    Первая рабочая версия

--- backend/core/src/main/java/ru/kuzmin/secret/core/bean/CategoryServiceImpl.java
+++ backend/core/src/main/java/ru/kuzmin/secret/core/bean/CategoryServiceImpl.java
@@ -1,39 +1,43 @@
 package ru.kuzmin.secret.core.bean;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 import ru.kuzmin.secret.core.bean.interfaces.CategoryService;
 import ru.kuzmin.secret.core.dao.CategoryDao;
 import ru.kuzmin.secret.core.entity.Category;
 
 import java.util.List;
 
+@Service
 public class CategoryServiceImpl implements CategoryService {
 
-    @Autowired
-    private CategoryDao categoryDao;
+    private final CategoryDao categoryDao;
 
+    @Autowired
     public CategoryServiceImpl(CategoryDao categoryDao) {
         this.categoryDao = categoryDao;
     }
 
     @Override
-    public long create(Category category) {
-        return 0;
+    public Category save(Category category) {
+        if (category.getId() == null) {
+            category = categoryDao.insert(category);
+        } else {
+            categoryDao.update(category);
+        }
+        return category;
     }
 
     @Override
-    public void update(Category category) {
-
+    public void delete(Category category) {
+        categoryDao.delete(category);
     }
 
     @Override
-    public void remove(Category category) {
-
-    }
-
-    @Override
-    public List<CategoryContent> getContent(Category parentCategory) {
-        return null;
+    public List<Category> getSubcategories(Category category) {
+        return category == null
+                ? categoryDao.loadRootCategories()
+                : categoryDao.loadSubcategories(category);
     }
 
 }
